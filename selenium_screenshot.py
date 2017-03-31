c3a28d7c3771742c09c4de26fb6fd249347c24da
WitnessQin <395139262@qq.com>
2017-03-31 16:00:49 +0800

    change

--- selenium_screenshot.py
+++ selenium_screenshot.py
@@ -2,23 +2,36 @@ from PIL import Image
 from selenium import webdriver
 import time
 
+global driver
+
 url='http://10.10.63.253:9080/secure/Dashboard.jspa?selectPageId=10172'
-driver=webdriver.Edge()
-driver.maximize_window()
-driver.get(url)
-time.sleep(3)
+image_path='d:\\test\\web.png'
+element_id='gadget-10730-chrome'
+element_image_path='d:\\test\\web-1.png'
+
 #拉起浏览器打开对应页面
-driver.save_screenshot('d:\\test\\web.png')
-#截图大图
-location=driver.find_element_by_id('gadget-10730-chrome').location
-size=driver.find_element_by_id('gadget-10730-chrome').size
-rangle=(int(location['x']),int(location['y']),int(location['x']+size['width']),int(location['y']+size['height']))
-#获取特定ID的元素的大小
-maxscreen=Image.open('d:\\test\\web.png')
-lastimage=maxscreen.crop(rangle)
-lastimage.save('d:\\test\\web-1.png')
-#从大图截取小图
-driver.execute_script("window.scrollBy(0,3000)")
-#滚动页面
+def openurl(url):
+	global driver
+	driver=webdriver.Edge()
+	driver.maximize_window()
+	driver.get(url)
+	time.sleep(3)
+	
+#根据id查找元素的大小，并对大图进行截取保存
+def catchscreeen(image_path,element_id,element_image_path):
+	mainimage=Image.open(image_path)
+	try:
+		location=driver.find_element_by_id(element_id).location
+		size=driver.find_element_by_id(element_id).size
+		print('find the element: '+element_id)
+	except Exception as e:
+		print('can not find the element: '+element_id)
+		raise e
+	rangle=(int(location['x']),int(location['y']),int(location['x']+size['width']),int(location['y']+size['height']))
+	lastimage=mainimage.crop(rangle)
+	lastimage.save(element_image_path)
+
+openurl(url)
+driver.save_screenshot(image_path)
+catchscreeen(image_path,element_id,element_image_path)
 driver.close()
-#关闭浏览器
